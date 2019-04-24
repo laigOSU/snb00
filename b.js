818fca810ae8c00d01a53282216353207839669b
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-04-24 01:53:39 -0700

    Create b.js

---
+++ b.js
@@ -0,0 +1,56 @@
+from flask import Blueprint, request
+from google.cloud import datastore
+import json
+import constants
+
+client = datastore.Client()
+
+bp = Blueprint('boat', __name__, url_prefix='/boats')
+
+@bp.route('/', methods=['POST','GET'])
+# def guests_get_post():
+def boats_get_post():
+    if request.method == 'POST':
+        content = request.get_json()
+        new_boat = datastore.entity.Entity(key=client.key(constants.boats))
+        new_boat.update({"name": content["name"], 'type': content['type'], 'length': content['length'], 'docked': 'null'})
+        client.put(new_boat)
+        return str(new_boat.key.id)
+    elif request.method == 'GET':
+        query = client.query(kind=constants.boats)
+        q_limit = int(request.args.get('limit', '2'))
+        q_offset = int(request.args.get('offset', '0'))
+        g_iterator = query.fetch(limit= q_limit, offset=q_offset)
+        pages = g_iterator.pages
+        results = list(next(pages))
+        if g_iterator.next_page_token:
+            next_offset = q_offset + q_limit
+            next_url = request.base_url + "?limit=" + str(q_limit) + "&offset=" + str(next_offset)
+        else:
+            next_url = None
+        for e in results:
+            e["id"] = e.key.id
+        output = {"boats": results}
+        if next_url:
+            output["next"] = next_url
+        return json.dumps(output)
+
+@bp.route('/<id>', methods=['PUT','DELETE','GET'])
+def boats_put_delete_get(id):
+    if request.method == 'PUT':
+        content = request.get_json()
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key)
+        boat.update({"name": content["name"]})
+        client.put(boat)
+        return ('',200)
+    elif request.method == 'DELETE':
+        key = client.key(constants.boats, int(id))
+        client.delete(key)
+        return ('',200)
+    elif request.method == 'GET':
+        my_boat_key = client.key(constants.boats, int(id))
+        requested_boat = client.get(key=my_boat_key)
+        return (json.dumps(requested_boat))
+    else:
+        return 'Method not recogonized'
