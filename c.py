9752a5ec57e4c548a210e6c4b3bf30ef16f2f36a
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-05-04 14:26:17 -0700

    load and unload c

--- c.py
+++ c.py
@@ -17,7 +17,9 @@ def cargos_get_post():
         client.put(new_cargo)
         cargo_id = str(new_cargo.key.id)
         url = constants.appspot_url + constants.cargos + "/" + cargo_id
-        new_cargo["self"] = url
+        new_cargo["cargo_url"] = url
+        client.put(new_cargo)
+        new_cargo["carrier"] = {"id": "null", "name": "null", "boat_url": "null"}
         client.put(new_cargo)
         return (str(new_cargo.key.id), 201)
 
@@ -51,23 +53,23 @@ def cargo_put_delete_get(id):
         # Get the cargo
         key = client.key(constants.cargos, int(id))
 
-        # Check if cargo is docked in a slip --> if cargo_id == slip["current_cargo"]
-        # Get that slip
-        query = client.query(kind=constants.slips)
-        query.add_filter('current_cargo', '=', id)
-        queryresults = list(query.fetch())
-        print("queryresults is: ", queryresults)
-        for e in queryresults:
-            print("number is: ", e["number"])
-            print("current_cargo is: ", e["current_cargo"])
-            print("slip id is: ", e.key.id)
-            slip_id = e.key.id
-
-            slip_key = client.key(constants.slips, slip_id)
-            slip = client.get(key=slip_key)
-            slip["current_cargo"] = "null"
-            slip["arrival_date"] = "null"
-            client.put(slip)
+        # # Check if cargo is docked in a slip --> if cargo_id == slip["current_cargo"]
+        # # Get that slip
+        # query = client.query(kind=constants.slips)
+        # query.add_filter('current_cargo', '=', id)
+        # queryresults = list(query.fetch())
+        # print("queryresults is: ", queryresults)
+        # for e in queryresults:
+        #     print("number is: ", e["number"])
+        #     print("current_cargo is: ", e["current_cargo"])
+        #     print("cargo id is: ", e.key.id)
+        #     slip_id = e.key.id
+        #
+        #     slip_key = client.key(constants.slips, slip_id)
+        #     slip = client.get(key=slip_key)
+        #     slip["current_cargo"] = "null"
+        #     slip["arrival_date"] = "null"
+        #     client.put(slip)
         client.delete(key)
 
         return ('',200)
@@ -75,14 +77,23 @@ def cargo_put_delete_get(id):
     #---- GET: VIEW A SPECIFIC CARGO ----#
     elif request.method == 'GET':
         query = client.query(kind=constants.cargos)
-        first_key = client.key(constants.cargos,int(id))
-        query.key_filter(first_key,'=')
+        cargo_key = client.key(constants.cargos,int(id))
+        query.key_filter(cargo_key,'=')
         results = list(query.fetch())
         for e in results:
             e["id"] = id
-            # url = "http://localhost:8080/cargos/" + id
-            url = constants.appspot_url + constants.cargos + "/" + id
-            e["cargo_url"] =url
+
+
+        # The below is for debugging --------
+        cargo_key = client.key(constants.cargos, int(id))
+        cargo = client.get(key=cargo_key)
+        print("cargo[carrier]: ", cargo["carrier"])
+        print("cargo[carrier][name]: ", cargo["carrier"]["name"])
+        print("cargo[carrier][id]: ", cargo["carrier"]["id"])
+        print("cargo[carrier][boat_url]: ", cargo["carrier"]["boat_url"])
+        print("cargo is", cargo)
+        # The above is for debugging --------
+
         return json.dumps(results)
 
 
